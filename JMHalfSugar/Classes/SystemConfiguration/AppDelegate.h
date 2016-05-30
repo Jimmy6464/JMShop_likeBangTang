b372545bf08609c110552a0b373c719ccd64fd4f
Jimmy <sz6464@163.com>
2016-05-30 17:18:31 +0800

    first commited

---
+++ JMHalfSugar/Classes/SystemConfiguration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JMHalfSugar
+//
+//  Created by Jimmy on 16/5/27.
+//  Copyright © 2016年 HDCircles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JMHalfSugar/Classes/SystemConfiguration/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JMHalfSugar
+//
+//  Created by Jimmy on 16/5/27.
+//  Copyright © 2016年 HDCircles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
